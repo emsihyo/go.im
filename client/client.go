04c6fb83b278cdf9d3530e626837e63e6f92920c
emsihyo <emsihyo@gmail.com>
2017-11-17 11:18:45 +0800

    no message

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"strings"
 	"sync"
@@ -42,7 +43,7 @@ func NewClient(addr string, id string, platform string) *Client {
 		// id := req.GetMessage().GetTo().GetID()
 		// if sid > cli.sids[id] {
 		// 	cli.sids[id] = sid
-		logrus.Debug("[REV]", req.GetMessage().GetTo().GetID(), req.GetMessage().GetFrom().GetID(), req.GetMessage().GetBody())
+		// logrus.Debug("[REV]", req.GetMessage().GetTo().GetID(), req.GetMessage().GetFrom().GetID(), req.GetMessage().GetBody())
 		// }
 	})
 	return &cli
@@ -52,6 +53,7 @@ func (cli *Client) preproccess() {
 	for {
 		if false == cli.isConnected {
 			if err := cli.connect(); nil != err {
+				log.Print(err)
 				<-time.After(time.Second * 3)
 				continue
 			} else {
@@ -60,15 +62,18 @@ func (cli *Client) preproccess() {
 		}
 		if false == cli.isLogged {
 			if err := cli.login(); nil != err {
+				log.Print(err)
 				<-time.After(time.Second * 3)
 				continue
 			} else {
 				cli.isLogged = true
 				for _, topicID := range cli.topics {
 					if _, err := cli.subscribe(topicID); nil != err {
+						log.Print(err)
 						cli.reset()
 						break
 					}
+					<-time.After(time.Millisecond * 10)
 				}
 				return
 			}
@@ -81,12 +86,12 @@ func (cli *Client) preproccess() {
 func (cli *Client) connect() error {
 	a, err := net.ResolveTCPAddr("tcp", cli.addr)
 	if nil != err {
-		logrus.Debug("[CONNECT]", cli.id, err)
+		log.Print(err)
 		return err
 	}
 	c, err := net.DialTCP("tcp", nil, a)
 	if nil != err {
-		logrus.Debug("[CONNECT]", cli.id, err)
+		log.Print(err)
 		return err
 	}
 	conn := bi.NewTCPConn(c)
@@ -100,15 +105,14 @@ func (cli *Client) login() error {
 	resp := pr.RespLogin{}
 	err := cli.sess.GetSessionImpl().Request(pr.Type_Login.String(), &pr.ReqLogin{UserID: cli.id, Password: cli.password, Platform: cli.platform}, &resp, time.Hour)
 	if nil != err {
-		logrus.Debug("[LOGIN]", cli.id, err)
+		log.Print(err)
 		return err
 	}
 	if 0 != resp.Code {
 		err = fmt.Errorf("code:%d, desc:%s", resp.Code, resp.Desc)
-		logrus.Debug("[LOGIN]", cli.id, err)
+		log.Print(err)
 		return err
 	}
-	logrus.Debug("[LOGIN]", cli.id)
 	return nil
 }
 
@@ -128,20 +132,20 @@ func (cli *Client) Subscribe(topicID string) {
 }
 
 //Unsubscribe Unsubscribe
-// func (cli *Client) Unsubscribe(topicID string) {
-// 	cli.mut.Lock()
-// 	defer cli.mut.Unlock()
-// 	for {
-// 		cli.preproccess()
-// 		err := cli.unsubscribe(topicID)
-// 		if nil != err {
-// 			logrus.Debug("[UNSUBSCRIBE]", err)
-// 			cli.reset()
-// 		} else {
-// 			return
-// 		}
-// 	}
-// }
+func (cli *Client) Unsubscribe(topicID string) {
+	cli.mut.Lock()
+	defer cli.mut.Unlock()
+	for {
+		cli.preproccess()
+		err := cli.unsubscribe(topicID)
+		if nil != err {
+			logrus.Debug("[UNSUBSCRIBE]", err)
+			cli.reset()
+		} else {
+			return
+		}
+	}
+}
 
 //Publish Publish
 func (cli *Client) Publish(topicID string, body string) {
@@ -181,52 +185,53 @@ func (cli *Client) subscribe(topicID string) ([]*pr.Message, error) {
 	resp := pr.RespSubscribe{}
 	err := cli.sess.GetSessionImpl().Request(pr.Type_Subscribe.String(), &pr.ReqSubscribe{TopicID: topicID, MinSID: sid, MaxCount: 20}, &resp, time.Hour)
 	if nil != err {
-		logrus.Debug("[SUBSCRIBE]", cli.id, topicID, err)
+		log.Print(err)
 		return nil, err
 	}
 	if 0 != resp.Code {
 		err = fmt.Errorf("code:%d, desc:%s", resp.Code, resp.Desc)
-		logrus.Debug("[SUBSCRIBE]", cli.id, topicID, err)
+		log.Print(err)
 		return nil, err
 	}
 	cli.topics[topicID] = topicID
 	if 0 != len(resp.Histories) {
 		cli.sids[topicID] = resp.Histories[len(resp.Histories)-1].GetSID()
 	}
-	logrus.Debug("[SUBSCRIBE]", cli.id, topicID)
 	return resp.Histories, nil
 }
 
-// func (cli *Client) unsubscribe(topicID string) error {
-// 	if _, ok := cli.topics[topicID]; !ok {
-// 		return nil
-// 	}
-// 	resp := pr.RespUnsubscribe{}
-// 	err := cli.sess.Request(int32(pr.Type_Unsubscribe), &pr.ReqUnsubscribe{TopicID: topicID}, &resp, time.Hour)
-// 	if nil != err {
-// 		return err
-// 	}
-// 	if 0 != resp.Code {
-// 		return fmt.Errorf("code:%d, desc:%s", resp.Code, resp.Desc)
-// 	}
-// 	delete(cli.topics, topicID)
-// 	return nil
-// }
+func (cli *Client) unsubscribe(topicID string) error {
+	if _, ok := cli.topics[topicID]; !ok {
+		return nil
+	}
+	resp := pr.RespUnsubscribe{}
+	err := cli.sess.GetSessionImpl().Request(pr.Type_Unsubscribe.String(), &pr.ReqUnsubscribe{TopicID: topicID}, &resp, time.Hour)
+	if nil != err {
+		log.Print(err)
+		return err
+	}
+	if 0 != resp.Code {
+		err := fmt.Errorf("code:%d, desc:%s", resp.Code, resp.Desc)
+		log.Print(err)
+		return err
+	}
+	delete(cli.topics, topicID)
+	return nil
+}
 
 func (cli *Client) publish(topicID string, body string) error {
 	message := pr.Message{CID: uuid.NewV3(uuid.NewV4(), topicID+"|"+cli.id).String(), To: &pr.Topic{ID: topicID}, Body: body, From: &pr.Consumer{ID: cli.id}}
 	resp := pr.RespDeliver{}
 	err := cli.sess.GetSessionImpl().Request(pr.Type_Deliver.String(), &pr.ReqDeliver{Message: &message}, &resp, time.Hour)
 	if nil != err {
-		logrus.Debug("[PUBLISH]", cli.id, topicID, body, err)
+		log.Print(err)
 		return err
 	}
 	if 0 != resp.Code {
 		err = fmt.Errorf("code:%d, desc:%s", resp.Code, resp.Desc)
-		logrus.Debug("[PUBLISH]", cli.id, topicID, body, err)
+		log.Print(err)
 		return err
 	}
-	logrus.Debug("[PUBLISH]", cli.id, topicID, body)
 	return nil
 }
 
@@ -235,12 +240,10 @@ func (cli *Client) ping() (int64, error) {
 	now := time.Now().UnixNano()
 	err := cli.sess.GetSessionImpl().Request(pr.Type_Ping.String(), &pr.ReqPing{}, &resp, time.Hour)
 	if nil != err {
-		logrus.Debug("[PING]", cli.id, err)
+		log.Print(err)
 		return 0, err
 	}
-	// delay := (time.Now().UnixNano() - now) / time.Millisecond.Nanoseconds()
-	delay := (time.Now().UnixNano() - now)
-	logrus.Debug("[PING]", cli.id, delay)
+	delay := (time.Now().UnixNano() - now) / int64(time.Millisecond)
 	return delay, nil
 }
 
